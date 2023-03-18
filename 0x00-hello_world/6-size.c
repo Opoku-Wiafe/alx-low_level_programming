0cbe93a97e3fba4fa1875edbe4ee550c5f177a86
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-18 02:56:25 -0700

    Data Type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 	char char_size;
 	float float_size;
 	long int long_int;
-	long long int l_long_int;
+	long long int a;
 
-	printf("Size of a char: %lu Byte(s)\n", sizeof(char_size));
-	printf("Size of an int: %lu Byte(s)\n", sizeof(int_size));
-	printf("Size of a long int: %lu Byte(s)\n", sizeof(long_int));
-	printf("Size of a long long int: %lu Byte(s)\n", sizeof(l_long_int));
-	printf("Size of a char: %lu Byte(s)\n", sizeof(float_size));
-	return (0);
+printf("Size of a char: %lu Byte(s)\n", (unsigned long)sizeof(char_size));
+printf("Size of an int: %lu Byte(s)\n", (unsigned long)sizeof(int_size));
+printf("Size of a long int: %lu Byte(s)\n", (unsigned long)sizeof(long_int));
+printf("Size of a long long int: %lu Byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu Byte(s)\n", (unsigned long)sizeof(float_size));
+return (0);
 }
