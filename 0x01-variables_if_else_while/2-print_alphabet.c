59bbaddb224ea6939acf0c626d49fb478255dd2d
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-19 02:29:09 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that displays Alphabets A to Z.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{	char a;
+
+	/*for loop is needed here */
+	for (a = 'a'; a <= 'z'; a++)
+	{	putchar(a);
+	}
+	return (0);
+}
