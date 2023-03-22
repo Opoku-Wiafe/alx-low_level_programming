c25ddf1e3782661a6affe784d13d40bef459fc5a
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-21 19:19:41 -0700

    10 Times the Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - This function displays the alphabet 10 times.
+ *
+ * Return: always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 25; j++)
+		{
+			_putchar(j + 'a');
+		}
+		_putchar('\n');
+	}
+}
