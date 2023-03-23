70ae88012c16279cbc6166fb8be35ee2edcfebef
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-23 10:57:17 -0700

    Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The main code that checks the multiple of 3 and 5.
+ *
+ * Return: always return 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 3 == 0))
+		{
+			j = j + i;
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
