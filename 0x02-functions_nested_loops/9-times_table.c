b0e1cd8a9b4e60c8c96a750df45bb2d4c3aca37f
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-23 12:25:07 -0700

    9 Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,39 @@
 #include "main.h"
 /**
+ * times_table - this function prints times table of 9.
+ *
+ * Return: return 0 since void.
  */
 void times_table(void)
 {
-	for ()
+	int i, j, k, l, d;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+
+			if (k > 9)
+			{
+				l = k % 10;
+				d = (k - l) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(l + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(k + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
