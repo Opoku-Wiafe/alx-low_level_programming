28d30b333f54489d5e7a35947988aad37c0774c8
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-25 15:48:34 -0700

    Is Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,23 @@
 #include "main.h"
+/**
+ * _isupper - is the function the main entry to the code
+ *
+ * @c: this is the parameter to check if its upper or lower.
+ *
+ * Return: 1 if upper case and 0 if lowercase.
+ */
+
+int _isupper(int c)
+{
+/*	for (c = 65; c <= 122; c++)*/
+	
+		if (c >= 65 && c <= 90)
+		{
+			return (1);
+		}
+	/*	else if (c >= 97 && c <= 122)*/
+		{
+			return (0);
+		}
+
+}
