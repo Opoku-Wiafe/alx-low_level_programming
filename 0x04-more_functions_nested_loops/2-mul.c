8be8f9499f763bc76403461aa040ab0607268014
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-25 16:44:26 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - this is the main function that multiply
+ *
+ * @a: first declare integer.
+ * @b: second declared integer
+ *
+ * Return: returns the product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
