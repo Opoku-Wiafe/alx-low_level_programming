1e822795f96f5de8f63c4f93109a06f8f476035d
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-25 16:16:06 -0700

    is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,9 @@
 
 int _isupper(int c)
 {
-/*	for (c = 65; c <= 122; c++)*/
-	
-		if (c >= 65 && c <= 90)
-		{
-			return (1);
-		}
-		else
-			return (0);
-
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+		return (0);
 }
