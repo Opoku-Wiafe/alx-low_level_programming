2fc05eccb6978f74027c2dc5ad64e8579ca14f72
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-25 17:12:34 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_numbers - This is the main function of the code
+ *
+ * Return: return i.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
