5dde671e4ef48673701b7414438cf8ba6f5f9023
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-27 16:40:05 -0700

    Changing Values

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * reset_to_98 - function to change value of pointer n
+ *
+ * @n: is the parameter
+ * Return: is an empty function.
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
