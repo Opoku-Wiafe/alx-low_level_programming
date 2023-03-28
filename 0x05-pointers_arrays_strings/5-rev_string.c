16905a677e7ad822741c3fdc935ef4774ec161d1
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-27 19:18:31 -0700

    String reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse or swap strings.
+ * @s: convert strings.
+ * Return: null or empty.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int c = 0;
+	char rv = s[0];
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
