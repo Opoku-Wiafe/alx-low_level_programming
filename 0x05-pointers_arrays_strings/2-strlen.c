f1b42c54f0465dbfbdb21f37b9c3ce0316f3701e
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-27 17:52:49 -0700

    String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: the pointer to the char s
+ * Return: the value of lengt.
+ */
+
+int _strlen(char *s)
+{
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+
+	}
+	return (len);
+
+}
