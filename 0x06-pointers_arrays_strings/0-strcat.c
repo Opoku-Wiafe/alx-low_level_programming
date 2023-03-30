1a83fd250efd5c861142b31126a63d3b5a22a7e5
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-29 18:31:50 -0700

    concatenate 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+ * _strcat - concatenate 2 strings
+ * @dest: first parameter
+ * @src: 2 parameter
+ * Return: return dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
