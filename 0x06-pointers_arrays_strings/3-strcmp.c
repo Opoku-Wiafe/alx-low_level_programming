db29a7ccade609a1102568c0e8a02333b036fab3
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-03-29 19:05:08 -0700

    compare 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,14 @@
 #include "main.h"
+/**
+ * _strcmp - compare 2 strings
+ * @s1: 1 placehold
+ * @s2: 2 placehold
+ * Return: return s1 and s2.
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 ==*s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
