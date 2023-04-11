4cfb41cda8f45bc017f634c72cd69bbd1aba92db
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-04-11 08:25:08 -0700

    Print The name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of code
+ *
+ * @argc: the number of times the command is passed
+ * @argv: Array of the code.
+ *
+ * Return: return 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
