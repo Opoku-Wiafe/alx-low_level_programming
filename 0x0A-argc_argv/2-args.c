1190113dc93cb4bda2c1339e9083ca6050a729be
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-04-11 10:29:46 -0700

    List all names

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program displays args one per line
+ * @argc: numbre of  args
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	(void)argc;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
