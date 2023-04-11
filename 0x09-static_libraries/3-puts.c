dcbcc89a3ca2003773a069063fbe5ca2074d659a
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-04-10 17:48:40 -0700

    go and push

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - displays string
+ * @str: Char to be converted.
+ *
+ * Return: returns null text
+ */
+
+void _puts(char *str)
+{
+	/*int len = 0;*/
+
+	while (*str != '\0')
+	{
+		/*len++;*/
+		_putchar(*str++);
+	}
+	_putchar('\n');
+
+}
