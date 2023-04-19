4bc25649e45601774d1eb9bef601b81d79f21fd2
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-04-19 02:57:41 -0700

    Struct Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ struct dog
 /**
  * dog_t - typedef of the structure dog
  */
-def struct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
