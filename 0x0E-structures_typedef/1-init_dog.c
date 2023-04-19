400359fe8bfdf97da36f1beb9f18386f2b195931
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-04-19 10:19:24 -0700

    Initiate the struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - function to initiate struct
+ * @d: struct of dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
