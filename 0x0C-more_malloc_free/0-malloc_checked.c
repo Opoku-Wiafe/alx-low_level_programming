60d05ba82a2965ba7baee5f7f396cd24a9641379
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-08-11 16:57:16 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of byte to be allocated
+ *
+ * Return: return pointer to the memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	*ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
