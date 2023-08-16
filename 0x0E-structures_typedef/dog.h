328019bfe5832e3d169d587ccdbe5f03cbf3962e
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-08-15 21:05:19 -0700

    structs and def 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - Structure for the datatype dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog.
+ *
+ * Description: info about dogs
+ */
+
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+
+
+
+#endif
