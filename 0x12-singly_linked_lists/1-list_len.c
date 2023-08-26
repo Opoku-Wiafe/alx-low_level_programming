65dd2ba9d2104d3926aa7a4866877005506e0868
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-08-25 22:01:59 -0700

    count Elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list that is being counted
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
