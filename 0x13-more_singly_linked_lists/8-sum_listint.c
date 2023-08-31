29b817227e174cd06eecdc7b01562a04f174fac2
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-08-30 19:09:38 -0700

    Add the Nth term

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - the sum of all the data (n) of a linked list.
+ * @head: head of the list
+ *
+ * Return: returns the sum of all the data (n) or 0 when empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum++;
+		sum = head->n;
+		head = head->next;
+	}
+	return (sum);
+}
