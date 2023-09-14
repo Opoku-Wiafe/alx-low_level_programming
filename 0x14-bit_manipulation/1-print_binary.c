cf5b3819cebcf9e4251611970e729b3ac39bf257
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-09-13 17:23:45 -0700

    Convert to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,49 @@
 #include "main.h"
+
+unsigned long int check_number(unsigned int b, unsigned int p);
 /**
  * print_binary - function that prints binary numbers
  * @n: The number to be converted.
- * Return: return Nothing
+ * Return: return Nothing.
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1;
+	unsigned long int i;
+	unsigned long int pass;
+	char terminate;
 
-	while (i <= n / 2)
+	terminate = 0;
+	i = check_number(2, sizeof(unsigned long int) * 8 - 1);
+	while (i != 0)
 	{
-		i *= 2;
-	}
-	while (i > 0)
-	{
-		if (n >= i)
+		pass = n & i;
+		if (pass == i)
 		{
+			terminate = 1;
 			_putchar('1');
-			n -= i;
 		}
-		else
+		else if (terminate == 1 || i == 1)
 		{
 			_putchar('0');
 		}
-		i /= 2;
+		i >>= 1;
 	}
+}
 
+/**
+ * check_number - determines number bases and ^power
+ * @b: base of the number
+ * @p: power of the number
+ *
+ * Return:  number bases or power
+ */
+unsigned long int check_number(unsigned int b, unsigned int p)
+{
+	unsigned long int j = 1;
+	unsigned int i = 1;
+
+	for (; i <= p; i++)
+		j *= b;
+	return (j);
 }
